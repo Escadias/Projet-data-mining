b9c531265b4771f5df604da6df5118432808068f
frederic <sn.fredo@gmail.com>
2017-06-26 13:40:40 +0200

    Stemming de gensim

--- Code/FinalProject.py
+++ Code/FinalProject.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
 #import csv
-from nltk.stem import PorterStemmer
+#from nltk.stem import PorterStemmer
 #from collections import OrderedDict
 import pandas
 import re
 import numpy
 import math
-import tensorflow as tf
+#import tensorflow as tf
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
+from gensim.models import word2vec
+import logging
+from StemmingHelper import StemmingHelper
 #from sklearn.naive_bayes import GaussianNB 
 
-porter=PorterStemmer()
+
+#porter=PorterStemmer()
                 
 
 
@@ -37,13 +42,18 @@ def deleteStopWords(listAllReview, listStopWords):
     for i in range(len(listAllReview)):
         review=str(listAllReview[i]).split()
         review=[word.lower() for word in review if word.lower() not in listStopWords]
-        listAllReview[i]=" ".join(review)
+        listAllReview[i]=" ".join(review)    
     return listAllReview
 
 
 
-
-
+def stemmingReviews(listAllReview):
+    for i in range(len(listAllReview)):
+        review=str(listAllReview[i]).split()
+        review=[StemmingHelper.stem(word.lower()) for word in review]
+        listAllReview[i]=" ".join(review)    
+    return listAllReview
+    
         
     
         
@@ -65,6 +75,7 @@ fileTampaxcsv=pandas.read_csv(fileTampax)
 frames = [fileAlwayscsv, fileGillettecsv, fileOralbcsv, filePantenecsv, fileTampaxcsv]
 alldataFrame = pandas.concat(frames)
 alldataFrame = alldataFrame.sample(frac= 1)
+
 listAllReview=list(alldataFrame['review'].astype(str))
 listAllRating=list(alldataFrame['user_rating'].astype(int))
 
@@ -73,6 +84,7 @@ for row in fileStopWords:
 
 listAllReview=replacePonctuation(listAllReview)
 listAllReview=deleteStopWords(listAllReview, listStopWords)
+listAllReview = stemmingReviews(listAllReview)
 nbLearning = round(len(listAllReview)*2/3)
 
 reviewLearning=listAllReview[:nbLearning]
@@ -109,32 +121,47 @@ matrixY=fillY(yDataFrame, listAllRating)
 
 matrixXLearn=matrixX[:nbLearning]
 matrixXTest=matrixX[nbLearning:]
+matrixYLearn=matrixY[:nbLearning]
 
-from sklearn.naive_bayes import GaussianNB
-model=GaussianNB().fit(matrixXLearn[list(matrixX)], rateLearning)
-predicted=model.predict(matrixXTest)
+#from sklearn.naive_bayes import GaussianNB
+#model=GaussianNB().fit(matrixXLearn[list(matrixX)], rateLearning)
+#predicted=model.predict(matrixXTest)
 
-def verifyPredict(predicted, rateTesting):
-    goodClassification=0
-    badClassification=0
-    nbClassification=0
-    for i in range (len(predicted)):
-        nbClassification+=1
-        if(predicted[i]==rateTesting[i]):
-            goodClassification+=1
-        else:
-            badClassification+=1
-    return [goodClassification/nbClassification, badClassification/nbClassification]
-
-listPredict=verifyPredict(predicted, rateTesting)
-print(listPredict)
-print(matrixX.shape)
-
-
-
+#def verifyPredict(predicted, rateTesting):
+#    goodClassification=0
+#    badClassification=0
+#    nbClassification=0
+#    for i in range (len(predicted)):
+#        nbClassification+=1
+#        if(predicted[i]==rateTesting[i]):
+#            goodClassification+=1
+#        else:
+#            badClassification+=1
+#    return [goodClassification/nbClassification, badClassification/nbClassification]
+#
+#listPredict=verifyPredict(predicted, rateTesting)
+#print(listPredict)
+#print(matrixX.shape)
 
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+
+
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                     hidden_layer_sizes=(5, 2), random_state=1)
+clf.fit(matrixXLearn, rateLearning)                         
+MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+       beta_1=0.9, beta_2=0.999, early_stopping=False,
+       epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
+       learning_rate_init=0.001, max_iter=200, momentum=0.9,
+       nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+       solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+       warm_start=False)
+predict = clf.predict(matrixXTest)
+print("Number of mislabeled points out of a total %d points : %d , efficiency : "
+      % (matrixXTest.shape[0],(rateTesting != predict).sum()), ((rateTesting == predict).sum())/matrixXTest.shape[0]*100)
 
 #import gensim
 #from gensim.keyedvectors import KeyedVectors
@@ -231,5 +258,6 @@ print(matrixX.shape)
 #dicProbScore3=computeProbWords(dicLearningScore3, nbWordsScore3)
 #dicProbScore4=computeProbWords(dicLearningScore4, nbWordsScore4)
 #dicProbScore5=computeProbWords(dicLearningScore5, nbWordsScore5)
+
 
 
